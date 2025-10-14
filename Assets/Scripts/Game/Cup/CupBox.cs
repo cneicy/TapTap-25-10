2db5a4d37a0c20b1f79b2f96f67e084cabece98e
cneicy <im@crash.work>
2025-10-14 19:01:33 +0800

    feat: cupbox

--- Assets/Scripts/Game/Cup/CupBox.cs
+++ Assets/Scripts/Game/Cup/CupBox.cs
@@ -1,9 +1,56 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using ShrinkEventBus;
+using UnityEngine;
+using Utils;
 
 namespace Game.Cup
 {
-    public class CupBox : MonoBehaviour
+    public class PlayerGetCupEvent : EventBase
     {
+        public CupBase Cup {  get; set; }
+        public PlayerGetCupEvent(CupBase cup)
+        {
+            Cup = cup;
+        }
+    }
+    [EventBusSubscriber]
+    public class CupBox : Singleton<CupBox>
+    {
+        public List<string> cupsPlayerHad = new();
         
+        
+        
+        private void OnEnable()
+        {
+            // todo:当玩家继续游戏时读取玩家数据拥有的奖杯
+            cupsPlayerHad.Add("Tap Cup");
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                RefreshCups();
+            }
+        }
+
+        public void RefreshCups()
+        {
+            foreach (var cupBase in GetComponentsInChildren<CupBase>())
+            {
+                // 留意大小写，小写的是gameobject的name
+                if (!cupsPlayerHad.Contains(cupBase.Name))
+                {
+                    cupBase.gameObject.SetActive(false);
+                }
+            }
+        }
+
+        [EventSubscribe]
+        public void OnPlayerGetCupEvent(PlayerGetCupEvent evt)
+        {
+            cupsPlayerHad.Add(evt.Cup.Name);
+        }
     }
 }
---
+++ Assets/Scripts/Game/Cup/CupItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Cup
+{
+    public class CupItem : MonoBehaviour
+    {
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                
+            }
+        }
+    }
+}
