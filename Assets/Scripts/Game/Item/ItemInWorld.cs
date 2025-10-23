abded6bba437b1b732dc2b6777da7c28ccdf7e54
cneicy <im@crash.work>
2025-10-23 18:57:51 +0800

    feat: soundmanager

--- Assets/Scripts/Game/Item/ItemInWorld.cs
+++ Assets/Scripts/Game/Item/ItemInWorld.cs
@@ -1,25 +1,43 @@
 ﻿using System;
 using System.Linq;
+using Game.Level;
 using ShrinkEventBus;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Game.Item
 {
     public class ItemInWorld : MonoBehaviour
     {
+        private static readonly int Jump = Animator.StringToHash("Jump");
         public string theItem;
-
         private void OnEnable()
         {
+            var manager = FindAnyObjectByType<LevelManager>();
+            if (manager is null)
+            {
+                if (SceneManager.GetActiveScene().name != "Entry")
+                {
+                    SceneManager.LoadScene("Entry");
+                    return;
+                }
+            }
             foreach (var unused in ItemSystem.Instance.ItemsPlayerHadTypeNames.Where(item => item == theItem))
             {
                 gameObject.SetActive(false);
             }
         }
 
+        private void FixedUpdate()
+        {
+            transform.Rotate(Vector3.up * (Time.fixedDeltaTime * 100), Space.Self);
+        }
+
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (!other.CompareTag("Player")) return;
+            SoundManager.Instance.Play("pickup");
+            other.GetComponentInChildren<Animator>().SetTrigger(Jump);
             EventBus.TriggerEvent(new PlayerGetItemEvent(theItem));
             gameObject.SetActive(false);
         }
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+
+public class SoundManager : Singleton<SoundManager>
+{
+    public AudioSource source;
+
+    [System.Serializable]
+    public struct Sound
+    {
+        public string name;
+        public AudioClip clip;
+    }
+
+    public List<Sound> sounds = new();
+    
+    public void Play(string name)
+    {
+        var s = sounds.Find(x => x.name == name);
+        if (s.clip)
+            source.PlayOneShot(s.clip);
+    }
+}
